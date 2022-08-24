8d6fc1aead5c379bd9ec450623cb60e8f5398967
RyanBeGell <Ryanbegell@outlook.com>
2022-08-24 14:08:28 -0400

    Toggle moves onClick, styling updated

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -3,17 +3,17 @@ import Image from 'next/image';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 
-export default function DarkModeToggle() {
+export default function DarkModeToggle(props: any) {
     return (
     <>
-        <div>
-            <input type="checkbox" className={styles.checkbox} id={styles.chk} />
-            <label className={styles.label} htmlFor="chk">
-                <Image src="/moon.svg" alt="moon" width={20} height={20}/>
-                <Image src="/sun.svg" alt="sun" width={20} height={20}/>
-                <div className={styles.ball}> </div>
-            </label>
-        </div>
+                {/* Conditionally render className (Toggle styling) depending on mode prop */}
+                <div className={props.mode ==='light'? styles.toggle: styles.toggleOn}> 
+                    <Image src="/moon.svg" alt="moon" width={20} height={20}/>
+                    <Image src="/sun.svg" alt="sun" width={20} height={20}/>
+
+                    {/* Ball inside of toggle, moves position depending on mode prop via CSS class*/}
+                    <div className={props.mode ==='light'?  styles.ballRight : styles.ball}/>
+            </div>
     </>
     )
 }
