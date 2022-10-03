103fa09ce2d511204a6766b368581bda03b4c5c1
RyanBeGell <Ryanbegell@outlook.com>
2022-10-03 09:34:51 -0400

    Blog section added with cards. CSS hover animation+shadow added

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions, Chip, Grid } from '@mui/material';
+import styles from '@/src/components/Card.module.css'
+import ImageListItem from '@mui/material/ImageListItem';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+
+export interface Props{
+    title?: string, //temporarily optional
+    body?: string, //temporarily optional
+    image?: string, //temporarily optional
+    chip1?: string,
+    chip2?: string,
+    chip3?: string,
+  }
+/*
+  This component is to be used within an MUI image list, each ProjectCard is an ImageListItem. 
+*/
+export default function ProjectCard(props:Props) {
+    return (<>
+    <Grid item xs={12} sm={6} md={4}>
+        <Card sx={{ maxWidth: 400 }} className={styles.card}>
+        <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image="https://animals.sandiegozoo.org/sites/default/files/2017-12/iguana-grand-cayman-blue.jpg"
+        />
+        <CardContent>
+        <Chip label="React" size='small' color='primary' variant='outlined'  sx={{mb:'16px'}}/>
+            <Typography gutterBottom variant="h5" component="div">
+            Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+            </Typography>
+        </CardContent>
+        <CardActions>
+            <Button>Read More <ArrowRightAltIcon/></Button>
+        </CardActions>
+        </Card>
+    </Grid>
+    </>)
+}
--- src/components/RecentBlogPosts.tsx
+++ src/components/RecentBlogPosts.tsx
@@ -1,7 +1,69 @@
-export default function RecentBlogPosts(){
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid'
+import { useState } from 'react';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LanguageIcon from '@mui/icons-material/Language';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import Modal from '@mui/material/Modal';
+import styles from '@/src/components/Projects.module.css'
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
+import BlogCard from './BlogCard';
+import Grow from '@mui/material/Grow';
+import Fade from '@mui/material/Fade';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import SectionTitle from './SectionTitle';
 
+export interface Props{
+    themeMode?: string, //Takes in themeMode prop to conditionally render button style 
+  }
+export default function RecentBlogPosts(props: Props){
 
     return(<>
-    
+    <Box className="centerBox">
+        <Grid>
+            <Grid item>
+                <SectionTitle title="Recent Blog Posts"/>
+            </Grid>
+            <Grid item>
+                <Grid
+                container
+                spacing={4}
+                sx={{ pb:'48px'}}
+                justifyContent="center"
+                >
+                    <BlogCard 
+                            title="Lizard"
+                            body="Lizards are a widespread group of squamate reptiles,
+                                with over 6,000 species, ranging across all continents except Antarctica"
+                        />
+                    <BlogCard 
+                            title="Lizard"
+                            body="Lizards are a widespread group of squamate reptiles,
+                                with over 6,000 species, ranging across all continents except Antarctica"
+                        />
+                    <BlogCard 
+                            title="Lizard"
+                            body="Lizards are a widespread group of squamate reptiles,
+                                with over 6,000 species, ranging across all continents except Antarctica"
+                        />
+                </Grid>
+            </Grid>
+            <Grid item justifyContent={'center'} textAlign='center'>
+                {/* Render button outlined in dark mode, contained in light mode */}
+                <Button size='large' variant={`${props.themeMode=='dark'?'outlined':'contained'}`}>
+                    View Blog &nbsp;<ArrowCircleRightIcon/>
+                </Button>
+            </Grid>
+        </Grid>
+    </Box>
     </>)
 }
