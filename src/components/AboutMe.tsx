fd250c28e72b82dbaa32d4c45703effd325d30ec
RyanBeGell <Ryanbegell@outlook.com>
2022-10-04 10:17:45 -0400

    AboutMe section

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,7 +1,12 @@
+import { Box } from "@mui/material";
+import SectionTitle from "./SectionTitle";
+
 export default function AboutMe(){
 
 
     return(<>
-    
+    <Box className="centerBox">
+        <SectionTitle title="About Me"/>
+    </Box>
     </>)
 }
