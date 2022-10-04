7621e7b2595536e0c3db57db17f01addeaac447c
RyanBeGell <Ryanbegell@outlook.com>
2022-10-04 10:18:37 -0400

    Contact form added

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,66 @@
+import Box from '@mui/material/Box';
+import SectionTitle from './SectionTitle';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+
 export default function Contact(){
 
-
     return(<>
-    
+    <Box className="centerBox">
+        <SectionTitle title="Get in Touch"  title2="Feel free to contact me anytime"/>
+        <Box component="form" noValidate >
+            <TextField
+                margin="normal"
+                id="First Name"
+                label="First Name"
+                name="First Name"
+                autoComplete='off'
+                autoFocus
+                sx={{width:'49%' ,mr:'2%'}}
+            />
+            <TextField
+                margin="normal"
+                id="Last Name"
+                label="Last Name"
+                name="Last Name"
+                autoComplete='off'
+                autoFocus
+                sx={{width:'49%' }}
+            />
+            <TextField
+                margin="normal"
+                fullWidth
+                autoFocus
+                autoComplete='off'
+                id="email"
+                label="Email Address"
+                name="email"
+            />
+            <TextField
+                margin="normal"
+                fullWidth
+                autoFocus
+                autoComplete='off'
+                id="subject"
+                label="Subject"
+                name="subject"
+            />
+            <TextField
+                margin="normal"
+                fullWidth
+                multiline
+                autoFocus
+                rows={6}
+                autoComplete='off'
+                id="message"
+                label="Your message here"
+                name="message"
+            />
+            <Button size='large' variant='outlined' sx={{mt:'16px'}} startIcon={<SendIcon/>}>
+             Send Message
+            </Button>
+          </Box>
+    </Box>
     </>)
 }
