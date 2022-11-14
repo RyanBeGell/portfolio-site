1cbe7cee9ae5425b515f4c2dbf7256179643dee7
RyanBeGell <Ryanbegell@outlook.com>
2022-11-14 08:06:53 -0500

    post information from post files using gray-matter

--- pages/blog/home.tsx
+++ pages/blog/home.tsx
@@ -1,3 +1,30 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+export async function getStaticProps() {
+  // Get all our posts
+  const files = fs.readdirSync('posts');
+
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace('.md', '');
+    const readFile = fs.readFileSync(`posts/${fileName}`, 'utf-8');
+    const { data: frontmatter } = matter(readFile);
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
 export default function Home() {
 
   return (<>
