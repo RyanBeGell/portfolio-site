c5e4ab65e08467b4c2daa4cfd05b2a124bc198c9
RyanBeGell <Ryanbegell@outlook.com>
2023-01-23 18:08:20 -0500

    Testing new horizontal blog card design

--- pages/blog/home.jsx
+++ pages/blog/home.jsx
@@ -1,11 +1,21 @@
 import BlogAppBar from "../../src/components/blog/BlogAppBar";
-import { Box, Grid } from "@mui/material";
 import BlogPostsCardData from '@/src/data/BlogPostCardData';
 import BlogCard from '@/src/components/blog/BlogCard';
+import { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import BlogCard2 from '@/src/components/blog/BlogCard2'
+import { Divider, Typography } from "@mui/material";
+
+
 export default function Home() {
   return (<>
       <Box className="centerBox">
-        <Grid sx={{ maxWidth: 1150 }}>
+      <Typography variant={'h3'} className={"name"}>
+							Ryan BeGell
+				</Typography>
+        <Divider sx={{mb:'32px', pt:'24px', borderBottomWidth:'1.5px' }}/>
+        {/* <Grid sx={{ maxWidth: 1150 }}>
           <Grid item>
             <Grid
               container
@@ -23,7 +33,12 @@ export default function Home() {
               ))}
             </Grid>
           </Grid>
-        </Grid>
+        </Grid> */}
+        <BlogCard2/>
+        <BlogCard2/>
+        <BlogCard2/>
+        <BlogCard2/>
       </Box>
+
   </>)
 }
