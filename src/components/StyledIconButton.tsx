1363798367969ad98782ae95f3b6f2a6d2a8ce9d
RyanBeGell <Ryanbegell@outlook.com>
2023-01-25 11:44:56 -0500

    tooltip prop added to app bar icon buttons

--- src/components/StyledIconButton.tsx
+++ src/components/StyledIconButton.tsx
@@ -1,28 +1,32 @@
+import { Tooltip } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 
 export interface Props {
   children?: any;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  title: String
 }
 
 export default function StyledIconButton(props: Props) {
   return (
     <>
-      <IconButton
-        color="primary"
-        onClick={props.onClick}
-        sx={{
-          borderRadius: '10px',
-          border: '1px solid',
-          borderColor: 'background.paperDivider',
-          mx: '5px',
-          '&:hover': {
-            borderColor: 'primary.main',
-          },
-        }}
-      >
-        {props.children}
-      </IconButton>
+      <Tooltip title={props.title} placement="bottom"  PopperProps={{ sx:{mt:'10px !important'} }}>
+        <IconButton
+          color="primary"
+          onClick={props.onClick}
+          sx={{
+            borderRadius: '10px',
+            border: '1px solid',
+            borderColor: 'background.paperDivider',
+            mx: '5px',
+            '&:hover': {
+              borderColor: 'primary.main',
+            },
+          }}
+        >
+          {props.children}
+        </IconButton>
+      </Tooltip>
     </>
   );
 }
