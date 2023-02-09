ea814bca7c8b1511fb3a858d5f9db1edc2c828a8
RyanBeGell <Ryanbegell@outlook.com>
2023-02-08 21:56:20 -0500

    index added for easy export of all icons

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,14 @@
+export { default as CSS3 } from './CSS3';
+export { default as HTML5 } from './HTML5';
+export { default as Java } from './Java';
+export { default as JavaScript } from './JavaScript';
+export { default as MaterialUI } from './MaterialUI';
+export { default as NextJS } from './Next';
+export { default as NodeJS } from './Node';
+export { default as OracleDB } from './Oracle';
+export { default as PostgreSQL } from './Postgres';
+export { default as ReactJS } from './React';
+export { default as Spring } from './Spring';
+export { default as TypeScript } from './TypeScript';
+export { default as Docker } from './Docker';
+export { default as AWS } from './AWS'
