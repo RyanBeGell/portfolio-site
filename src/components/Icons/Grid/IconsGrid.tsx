8fd9517e4415fa86cb6e0c006c57b53bcdd0425b
RyanBeGell <Ryanbegell@outlook.com>
2023-02-23 10:19:14 -0500

    unnecessary outer div removed

--- src/components/Icons/Grid/IconsGrid.tsx
+++ src/components/Icons/Grid/IconsGrid.tsx
@@ -1,5 +1,5 @@
 import * as Icons from '@/src/components/Icons';
-import { Box, Divider, Grid, Typography } from '@mui/material';
+import { Divider, Grid, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import React from 'react';
 
@@ -23,33 +23,31 @@ const IconsGrid: React.FC<Props> = ({
 
   return (
     <Grid container spacing={spacing}>
-      {componentNames.map((name,index) => {
+      {componentNames.map((name, index) => {
         const DynamicComponent = (Icons as Record<string, any>)[name];
         return (
-          //index is stable here, this array doesn't change after build time
-          <Box key={index} >
-            <Grid textAlign="center">
-              <DynamicComponent
-                height={height}
-                width={width}
-                fill={primary}
-              />
-              {!noTitle && (
-                <>
-                  <Divider
-                    sx={{
-                      bgcolor: (theme) => theme.palette.primary.main,
-                      height: 2,
-                      my: 1,
-                    }}
-                  />
-                  <Typography variant={'body1'} sx={{ color: 'primary.main' }}>
-                    {name}
-                  </Typography>
-                </>
-              )}
-            </Grid>
-          </Box>
+          <Grid item textAlign="center" key={index}>
+            <DynamicComponent
+              key={name}
+              height={height}
+              width={width}
+              fill={primary}
+            />
+            {!noTitle && (
+              <>
+                <Divider
+                  sx={{
+                    bgcolor: (theme) => theme.palette.primary.main,
+                    height: 2,
+                    my: 1,
+                  }}
+                />
+                <Typography variant={'body1'} sx={{ color: 'primary.main' }}>
+                  {name}
+                </Typography>
+              </>
+            )}
+          </Grid>
         );
       })}
     </Grid>
