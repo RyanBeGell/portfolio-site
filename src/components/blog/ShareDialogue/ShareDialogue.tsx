6953a87c7a7c3da02203df8b549000907a27088f
RyanBeGell <Ryanbegell@outlook.com>
2023-02-27 20:33:15 -0500

    Functioning close button on modal

--- src/components/blog/ShareDialogue/ShareDialogue.tsx
+++ src/components/blog/ShareDialogue/ShareDialogue.tsx
@@ -1,50 +1,66 @@
-import * as React from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import RedditIcon from '@mui/icons-material/Reddit';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import LinkIcon from '@mui/icons-material/Link';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import RedditIcon from '@mui/icons-material/Reddit';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import IconButton from '@mui/material/IconButton';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import TextField from '@mui/material/TextField';
 
 export interface Props {
   open: boolean;
   handleClose: () => void;
 }
 
-export default function ShareDialog(props:Props) {
-  
+export default function ShareDialog(props: Props) {
   return (
     <>
       <Dialog open={props.open} onClose={props.handleClose}>
-        <DialogTitle>Share</DialogTitle>
-        <Divider sx={{mx:'24px'}}/>
+        <DialogTitle>
+          Share
+          <IconButton
+            onClick={props.handleClose}
+            sx={{ float: 'right', color:'text.secondary', ':hover': { color: 'red' } }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
+        <Divider sx={{ mx: '24px' }} />
         <DialogContent>
-          <DialogContentText sx={{mb:'16px'}}>
+          <DialogContentText sx={{ mb: '16px' }}>
             Share this link via
           </DialogContentText>
-          <IconButton size='large' sx={{border:'1px solid', mr:'8px', color:'#0077b5', fill:'white'}}>
-              <LinkedInIcon fontSize='inherit'/>
+          <IconButton
+            size="large"
+            sx={{ border: '1px solid', mr: '8px', color: '#0077b5' }}
+          >
+            <LinkedInIcon fontSize="inherit" />
           </IconButton>
-          <IconButton size='large'  sx={{border:'1px solid', mx:'8px', color: '#1DA1F2'}}>
-              <TwitterIcon fontSize='inherit'/>
+          <IconButton
+            size="large"
+            sx={{ border: '1px solid', mx: '8px', color: '#1DA1F2' }}
+          >
+            <TwitterIcon fontSize="inherit" />
           </IconButton>
-          <IconButton size='large'sx={{border:'1px solid', mx:'8px', color: '#FF5700'}}>
-              <RedditIcon fontSize='inherit'/>
+          <IconButton
+            size="large"
+            sx={{ border: '1px solid', mx: '8px', color: '#FF5700' }}
+          >
+            <RedditIcon fontSize="inherit" />
           </IconButton>
-          <IconButton size='large' sx={{border:'1px solid', mx:'8px', color:'#25D366'}}>
-            <WhatsAppIcon fontSize='inherit'/>
+          <IconButton
+            size="large"
+            sx={{ border: '1px solid', mx: '8px', color: '#25D366' }}
+          >
+            <WhatsAppIcon fontSize="inherit" />
           </IconButton>
-          <DialogContentText sx={{my:'16px'}}>
-            Copy Link
-          </DialogContentText>
+          <DialogContentText sx={{ my: '16px' }}>Copy Link</DialogContentText>
           <TextField
             autoFocus
             margin="dense"
