a91f5a5690e2821cf10e9094448c189171b1d1ea
RyanBeGell <Ryanbegell@outlook.com>
2023-03-04 18:31:51 -0500

    CSS transitions/styling for icons

--- src/components/Icons/Grid/IconsGrid.tsx
+++ src/components/Icons/Grid/IconsGrid.tsx
@@ -26,41 +26,49 @@ const IconsGrid: React.FC<Props> = ({
       {componentNames.map((name, index) => {
         const DynamicComponent = (Icons as Record<string, any>)[name];
         return (
-          <Grid item textAlign="center" key={index}>
-            {/* If no title, render a tooltip to show what the icon is on hover */}
-            {noTitle && (
-              <Tooltip title={name}>
-                {/* Div(Box) allows for tooltip to work */}
-                <Box>
+          <Grid item textAlign="center" key={index} sx={{ width: '120px' }}>
+            <Box className="trigger-icons" sx={{ cursor: 'pointer' }}>
+              {/* If no title, render a tooltip to show what the icon is on hover */}
+              {noTitle && (
+                <Tooltip title={name}>
+                  {/* Div(Box) allows for tooltip to work */}
+                  <Box className="third">
+                    <DynamicComponent
+                      key={name}
+                      height={height}
+                      width={width}
+                      fill={primary}
+                    />
+                  </Box>
+                </Tooltip>
+              )}
+              {!noTitle && (
+                <>
                   <DynamicComponent
                     key={name}
                     height={height}
                     width={width}
                     fill={primary}
                   />
-                </Box>
-              </Tooltip>
-            )}
-            {!noTitle && (
-              <>
-                <DynamicComponent
-                  key={name}
-                  height={height}
-                  width={width}
-                  fill={primary}
-                />
-                <Divider
-                  sx={{
-                    bgcolor: (theme) => theme.palette.primary.main,
-                    height: 2,
-                    my: 1,
-                  }}
-                />
-                <Typography variant={'body1'} sx={{ color: 'primary.main' }}>
-                  {name}
-                </Typography>
-              </>
-            )}
+                  <Divider
+                    className="second"
+                    sx={{
+                      bgcolor: (theme) => theme.palette.primary.main,
+                      height: 2,
+                      my: 1,
+                      mx: '20%',
+                    }}
+                  />
+                  <Typography
+                    className="first"
+                    variant={'body1'}
+                    sx={{ color: 'primary.main' }}
+                  >
+                    {name}
+                  </Typography>
+                </>
+              )}
+            </Box>
           </Grid>
         );
       })}
