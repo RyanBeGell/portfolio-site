ce66fa527e156c5d91a941faa71cfc42aa813509
RyanBeGell <Ryanbegell@outlook.com>
2023-03-05 14:34:25 -0500

    now pulls last 4 blog posts from data file

--- src/components/portfolio/RecentBlogPosts/RecentBlogPosts.tsx
+++ src/components/portfolio/RecentBlogPosts/RecentBlogPosts.tsx
@@ -5,8 +5,8 @@ import Grid from '@mui/material/Grid';
 import { useRouter } from 'next/router';
 import { useContext } from 'react';
 import { ColorModeContext } from '../../../../pages/_app';
-import BlogPostCardData from '../../blog/Card/BlogCardData';
 import BlogCard from '../../blog/Card/BlogCard';
+import BlogPostCardData from '../../blog/Card/BlogCardData';
 import SectionTitle from '../SectionTitle/SectionTitle';
 
 export interface Props {
@@ -21,11 +21,8 @@ export default function RecentBlogPostsSection(props: Props) {
     router.push('/blog/home');
   };
 
-  //Get the information from the last 3 blog posts from the data file
-  const RecentBlogPosts = BlogPostCardData.slice(
-    BlogPostCardData.length - 3,
-    BlogPostCardData.length
-  );
+  //Get the information from the last 4 blog posts from the data file
+  const RecentBlogPosts = BlogPostCardData.slice(0, 4);
 
   return (
     <>
@@ -35,43 +32,38 @@ export default function RecentBlogPostsSection(props: Props) {
             <SectionTitle title="Recent Blog Posts" />
           </Box>
           <Box sx={{ pb: '48px' }}>
-            <Grid item container xs={12} sx={{ width: '100%' }}>
-              <Grid
-                item
-                xl={6}
-                lg={6}
-                md={12}
-                sx={{ pb: '12px', pr: { xl: '12px', lg: '12px' } }}
-              >
-                <BlogCard />
-              </Grid>
-              <Grid
-                item
-                xl={6}
-                lg={6}
-                md={12}
-                sx={{ pb: '12px', pl: { xl: '12px', lg: '12px' } }}
-              >
-                <BlogCard />
-              </Grid>
-              <Grid
-                item
-                xl={6}
-                lg={6}
-                md={12}
-                sx={{ pt: '12px', pr: { xl: '12px', lg: '12px' } }}
-              >
-                <BlogCard />
-              </Grid>
-              <Grid
-                item
-                xl={6}
-                lg={6}
-                md={12}
-                sx={{ pt: '12px', pl: { xl: '12px', lg: '12px' } }}
-              >
-                <BlogCard />
-              </Grid>
+            <Grid item container xs={12} sx={{ width: '100%' }} >
+              {RecentBlogPosts.map((post, index) => (
+                <Grid
+                  item
+                  xl={6}
+                  lg={6}
+                  md={12}
+                  sx={{
+                    pb: index < 2 ? '12px' : '0px',
+                    pt: index >= 2 ? '12px' : '0px',
+                    pr:
+                      (index + 1) % 2 === 0
+                        ? { xl: '0px', lg: '0px' }
+                        : { xl: '12px', lg: '12px' },
+                    pl:
+                      (index + 1) % 2 === 0
+                        ? { xl: '12px', lg: '12px' }
+                        : { xl: '0px', lg: '0px' },
+                  }}
+                >
+                  <BlogCard
+                    key={index}
+                    title={post.title}
+                    body={post.body}
+                    image={post.image}
+                    path={post.path}
+                    date={post.date}
+                    chips={post.chips}
+                    minsToRead={post.minsToRead}
+                  />
+                </Grid>
+              ))}
             </Grid>
           </Box>
           <Grid item justifyContent={'center'} textAlign="center">
