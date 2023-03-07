5fbb502dd568bf676f235d415c41759131c9936c
RyanBeGell <Ryanbegell@outlook.com>
2023-03-07 08:42:06 -0500

    hover effect added to cert icons

--- src/components/portfolio/certifications/Certifications.tsx
+++ src/components/portfolio/certifications/Certifications.tsx
@@ -1,32 +1,35 @@
 import { Box, Grid } from '@mui/material';
 import Image from 'next/image';
 import SectionTitle from '../SectionTitle/SectionTitle';
-
+import styles from './Certifications.module.css'
 export default function Certifications() {
   return (
     <>
       <Box className="centerBox">
         <SectionTitle title="Certifications" />
-        <Grid container direction="row">
+        <Grid container direction="row" >
           <Grid container justifyContent="center" item xs={4}>
-            <Image
+            <img
               src="/AWS-Certified-Cloud-Practitioner.png"
               height="200"
               width="200"
+              className={styles.cert} 
             />
           </Grid>
           <Grid container justifyContent="center" item xs={4}>
-            <Image
+          <img
               src="/AWS-Certified-Developer-Associate_badge.png"
               height="200"
               width="200"
+              className={styles.cert} 
             />
           </Grid>
-          <Grid container justifyContent="center" item xs={4}>
-            <Image
+          <Grid container justifyContent="center" item xs={4} >
+          <img
               src="/AWS-Certified-Cloud-Practitioner.png"
               height="200"
               width="200"
+              className={styles.cert} 
             />
           </Grid>
         </Grid>
