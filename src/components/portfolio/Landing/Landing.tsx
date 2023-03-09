bb405a669adbd3b27ecd040c49f2bfbe46f01497
RyanBeGell <Ryanbegell@outlook.com>
2023-03-09 18:28:25 -0500

    react scroll tweak for mobile

--- src/components/portfolio/Landing/Landing.tsx
+++ src/components/portfolio/Landing/Landing.tsx
@@ -1,56 +1,64 @@
-import { scrollToAbout } from '@/src/components/portfolio/Sidebar/react-scroll/scrollers';
 import SouthIcon from '@mui/icons-material/South';
 import { Box, Grid, Typography } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import Image from 'next/image';
+import { scroller } from 'react-scroll';
 import ReactTyped from 'react-typed';
 import styles from './Landing.module.css';
 
+const handleScroll = () => {
+  //used to determine offset (mobile app bar is 56px height)
+  const mobile = window.innerWidth < 600;
+
+  scroller.scrollTo('skills', {
+    duration: 500,
+    delay: 100,
+    smooth: true,
+    offset: mobile ? -56 : 0,
+  });
+};
+
 export default function Landing() {
   return (
-      <Box className="centerFlexBox" sx={{ minHeight: '100vh', mx:'48px' }}>
-        <Grid  className="centerFlexBox">
-          <Grid item>
-            <Typography variant={'h1'} className={'name'}>
-              Ryan
-              <Typography
-                component="span"
-                variant={'h1'}
-                className={'name'}
-                color="primary"
-              >
-                &nbsp;BeGell
-              </Typography>
+    <Box className="centerFlexBox" sx={{ minHeight: '100vh', mx: '48px' }}>
+      <Grid className="centerFlexBox">
+        <Grid item>
+          <Typography variant={'h1'} className={'name'}>
+            Ryan
+            <Typography
+              component="span"
+              variant={'h1'}
+              className={'name'}
+              color="primary"
+            >
+              &nbsp;BeGell
             </Typography>
-            <Typography variant={'h4'} noWrap>
-              <ReactTyped
-                loop={false}
-                typeSpeed={80}
-                strings={['Full Stack Software Developer']}
-                smartBackspace={false}
-                shuffle={false}
-                loopCount={0}
-                showCursor={true}
-                cursorChar="_"
-              />
-            </Typography>
-          </Grid>
-          <Grid item sx={{ pl: '32px' }} >
-            <Image
-              src="/programming.svg"
-              alt="ManAtDesk"
-              width={700}
-              height={600}
+          </Typography>
+          <Typography variant={'h4'} noWrap>
+            <ReactTyped
+              loop={false}
+              typeSpeed={80}
+              strings={['Full Stack Software Developer']}
+              smartBackspace={false}
+              shuffle={false}
+              loopCount={0}
+              showCursor={true}
+              cursorChar="_"
             />
-          </Grid>
+          </Typography>
         </Grid>
-        <IconButton
-          id={styles.ArrowIcon}
-          color="primary"
-          onClick={scrollToAbout}
-        >
-          <SouthIcon color="primary" sx={{ fontSize: '32px' }} />
-        </IconButton>
-      </Box>
+        <Grid item sx={{ pl: '32px' }}>
+          <Image
+            src="/programming.svg"
+            alt="ManAtDesk"
+            width={700}
+            height={600}
+          />
+        </Grid>
+      </Grid>
+      <IconButton id={styles.ArrowIcon} color="primary" onClick={handleScroll}>
+        <SouthIcon color="primary" sx={{ fontSize: '32px' }} />
+      </IconButton>
+    </Box>
   );
 }
