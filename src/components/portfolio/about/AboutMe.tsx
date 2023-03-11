c2ba7d17937f2d8e8a4c203dd5c475c71323cfc3
RyanBeGell <Ryanbegell@outlook.com>
2023-03-11 17:52:35 -0500

    sample data

--- src/components/portfolio/about/AboutMe.tsx
+++ src/components/portfolio/about/AboutMe.tsx
@@ -6,6 +6,7 @@ export default function AboutMe() {
     <>
       <Box className="centerBox">
         <SectionTitle title="About Me" />
+        {"This is sample text. ".repeat(20)}
       </Box>
     </>
   );
