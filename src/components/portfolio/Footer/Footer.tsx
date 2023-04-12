b887548ff984227692893d261d72eaa9bc5ab21e
RyanBeGell <Ryanbegell@outlook.com>
2023-04-11 23:10:09 -0400

    footer styling enhancements

--- src/components/portfolio/Footer/Footer.tsx
+++ src/components/portfolio/Footer/Footer.tsx
@@ -1,15 +1,27 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Divider, Typography } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
 import FooterIcon from './FooterIcon';
-
 const Footer = () => {
   const year: number = new Date().getFullYear();
 
   return (
-    <Box display="flex" justifyContent="space-between" alignItems={'center'} >
-      <Typography variant={'subtitle2'} color="text.navFooter">
-        &copy; {year} Ryan BeGell
-      </Typography>
-      <Box display='flex'>
+    <Box display="flex" justifyContent="space-between" alignItems={'center'}>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Link href="/">
+          <Image height={25} width={25} src="/favicon.png" style={{ cursor: 'pointer' }}/>
+        </Link>
+          <Divider orientation="vertical" />
+        <Divider
+          orientation="vertical"
+          flexItem
+          sx={{ mx: '8px', my: '1px' }}
+        />
+        <Typography variant={'subtitle2'} color="text.navFooter">
+          &copy; {year} Ryan BeGell
+        </Typography>
+      </Box>
+      <Box display="flex">
         <FooterIcon type="GitHub" />
         <FooterIcon type="LinkedIn" color="linkedInBlue" />
         <FooterIcon type="Email" color="gmailRed" />
--- src/components/portfolio/Sidebar/Footer/SidebarFooter.tsx
+++ src/components/portfolio/Sidebar/Footer/SidebarFooter.tsx
@@ -1,16 +1,23 @@
 import { Box, Typography } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const SidebarFooter = () => {
   const year: number = new Date().getFullYear();
 
   return (
+    <>
       <Typography
         variant={'subtitle2'}
         color="text.navFooter"
-        sx={{ pt: '16px', width:'100%', textAlign:'center' }}
+        sx={{ pt: '16px', width:'100%', textAlign:'center',display: 'flex', alignItems:'center', justifyContent:'center' }}
       >
-        &copy; {year} Ryan BeGell
+        <Link href="/">
+          <Image height={22} width={22} src="/favicon.png" />
+        </Link>
+        &nbsp;&nbsp;&copy; {year} Ryan BeGell
       </Typography>
+      </>
   );
 };
 
