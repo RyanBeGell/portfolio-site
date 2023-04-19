604bbdb9290c3c2cb739e51850a1cb5b23f4605b
RyanBeGell <Ryanbegell@outlook.com>
2023-04-18 21:14:54 -0400

    Lorem ipsum placeholder text

--- src/components/portfolio/about/AboutMe.tsx
+++ src/components/portfolio/about/AboutMe.tsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
     <>
       <Box className="centerBox">
         <SectionTitle title="About Me" />
-        {"This is sample text. ".repeat(20)}
+        {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida elit sit amet mauris consequat ullamcorper. Nulla sed molestie purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer leo felis, mattis quis dui vel, eleifend placerat purus. Etiam rhoncus odio metus, ut commodo sem sodales ac. Vivamus purus metus, varius pharetra maximus vitae, pellentesque nec lacus. Nullam condimentum ut augue at posuere. Praesent mattis justo turpis, in ullamcorper purus sagittis non. Sed tellus dolor, blandit eget ante a, convallis volutpat massa. "}
       </Box>
     </>
   );
