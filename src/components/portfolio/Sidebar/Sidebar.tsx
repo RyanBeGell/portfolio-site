a11aecb12baaede93a3f9a8f911cc9075efcf73d
RyanBeGell <Ryanbegell@outlook.com>
2023-04-28 19:04:48 -0400

    sidebar button styling tweak

--- src/components/portfolio/Sidebar/Sidebar.tsx
+++ src/components/portfolio/Sidebar/Sidebar.tsx
@@ -155,7 +155,7 @@ export default function Sidebar(props: Props) {
           <ListItem key={text} sx={{ p: 0 }}>
             <ListItemButton
               sx={{
-                borderRadius: '10px',
+                borderRadius: '8px',
                 color: 'primary.main',
                 my: '2px',
                 py: '6px',
