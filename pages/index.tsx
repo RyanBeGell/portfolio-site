21433cd583ae32ccb95b0fac5769ff2263573a61
RyanBeGell <Ryanbegell@outlook.com>
2023-04-28 18:46:03 -0400

    floating scroll to top button added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,56 @@
-import AboutMe from '@/src/components/portfolio/about/AboutMe';
 import Certifications from '@/src/components/portfolio/Certifications/Certifications';
 import Contact from '@/src/components/portfolio/Contact/Contact';
-import Footer from '@/src/components/portfolio/Footer/Footer';
 import ProjectsSection from '@/src/components/portfolio/Projects/Projects';
 import RecentBlogPostsSection from '@/src/components/portfolio/RecentBlogPosts/RecentBlogPosts';
 import Skills from '@/src/components/portfolio/Skills/Skills';
-import { Box, Divider } from '@mui/material';
+import AboutMe from '@/src/components/portfolio/about/AboutMe';
+import KeyboardArrowUpRoundedIcon from '@mui/icons-material/KeyboardArrowUpRounded';
+import { Box, Divider, Fade, Tooltip } from '@mui/material';
+import Fab from '@mui/material/Fab';
 import type { NextPage } from 'next';
-import { useContext } from 'react';
-import { Element } from 'react-scroll';
-import { ColorModeContext } from './_app';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { Element, scroller } from 'react-scroll';
 
 const Home: NextPage = () => {
-  const { mode } = useContext(ColorModeContext);
+  const { pathname } = useRouter();
+  const [showScroll, setShowScroll] = useState(false);
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 200) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 200) {
+      setShowScroll(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScrollTop); // add checkScrollTop function reference here
+    return () => {
+      window.removeEventListener('scroll', checkScrollTop); // pass same function reference here
+    };
+  }, [checkScrollTop]); // include checkScrollTop function reference in dependencies array
+
+  //Set scroll history behavior to cause page to scroll to top on refresh
+  useEffect(() => {
+    if (pathname === '/') {
+      if ('scrollRestoration' in history) {
+        history.scrollRestoration = 'manual';
+      }
+      window.scrollTo(0, 0);
+    }
+    //Cleanup  function to reset scroll history behavior back to default
+    return () => {
+      if ('scrollRestoration' in history) {
+        history.scrollRestoration = 'auto';
+      }
+    };
+  }, [pathname]);
+
+  const handleScrollArrowClick = () => {
+    setShowScroll(false);
+    window.scrollTo({ top: 0});
+  };
 
   return (
     <>
@@ -40,6 +78,21 @@ const Home: NextPage = () => {
         <Element name="contact">
           <Contact />
         </Element>
+        <Fade timeout={500} in={showScroll} unmountOnExit >
+          <Fab
+            color="primary"
+            size="small"
+            aria-label="add"
+            onClick={handleScrollArrowClick}
+            sx={{
+              position: 'fixed',
+              bottom: '16px',
+              right: '16px',
+            }}
+          >
+            <KeyboardArrowUpRoundedIcon />
+          </Fab>
+        </Fade>
       </Box>
     </>
   );
