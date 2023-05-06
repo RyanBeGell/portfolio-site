ccacb20371774f2db26a241aee6eda8f7bf8cdd0
RyanBeGell <Ryanbegell@outlook.com>
2023-05-05 22:59:13 -0400

    unused social icon component removed

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,7 +2,6 @@ import BlogCard from '@/src/components/blog/Card/BlogCard';
 import BlogPostsCardData from '@/src/components/blog/Card/BlogCardData';
 import {
   Divider,
-  IconButton,
   OutlinedInput,
   Paper,
   Typography,
@@ -57,48 +56,21 @@ export default function Home() {
     : BlogPostsCardData;
 
   const darkMode = theme.palette.mode === 'dark';
-  const CodePenIcon = createSvgIcon(
-    <svg viewBox="0 0 128 128">
-      <path d="M125.571 39.926l-58.5-39c-1.997-1.23-4.128-1.24-6.142 0l-58.5 39C.929 40.926 0 42.712 0 44.497v39c0 1.786.929 3.572 2.429 4.571l58.5 39.006c1.996 1.229 4.128 1.24 6.142 0l58.5-39.006c1.5-.999 2.429-2.785 2.429-4.57v-39c0-1.786-.929-3.572-2.429-4.572zm-56.07-24.144l43.07 28.715-19.214 12.858L69.5 41.425V15.784zm-11.001 0v25.644L34.642 57.354 15.428 44.498 58.5 15.782zm-47.5 39l13.786 9.215L11 73.212v-18.43zm47.5 57.43L15.428 83.497 34.642 70.64 58.5 86.569v25.643zM64 76.997l-19.428-13 19.428-13 19.428 13-19.428 13zm5.5 35.215V86.569L93.357 70.64l19.214 12.857-43.07 28.715zm47.5-39l-13.786-9.215L117 54.783v18.429z" />
-    </svg>,
-    'CodePenIcon'
-  );
-
-  //sidebar social icons
-  const SocialIconButton = ({ Icon }: { Icon: React.ElementType }) => {
-    const isDarkMode = theme.palette.mode === 'dark';
-    const backgroundColor = isDarkMode ? theme.palette.primary.main : '#FFFFFF';
-    const color = isDarkMode ? 'white' : theme.palette.primary.main;
-    const hoverBackgroundColor = isDarkMode ? '' : 'rgba(0, 0, 0, 0.7)';
-
-    return (
-      <IconButton
-        sx={{
-          color,
-          backgroundColor,
-          ':hover': {
-            backgroundColor: hoverBackgroundColor,
-            color: 'primary.main',
-          },
-        }}
-      >
-        <Icon />
-      </IconButton>
-    );
-  };
 
   return (
     <>
       <Box className="centerBox" sx={{ mt: '64px', minHeight: '100vh' }}>
         <Box sx={{ display: 'flex', justifyContent: 'center' }}>
           <Box sx={{ maxWidth: '1000px', width: '100%', mx: 4 }}>
-            <Typography
-              variant={'h3'}
-              className={'name'}
-            >
+            <Typography variant={'h3'} className={'name'}>
               Dev Blog
             </Typography>
-            <Grid container spacing={5} alignItems="flex-start"sx={{pt:'24px'}}>
+            <Grid
+              container
+              spacing={5}
+              alignItems="flex-start"
+              sx={{ pt: '24px' }}
+            >
               <Grid item container xs={12} md={8} spacing={3}>
                 <Grid item xs={12} sx={{ maxHeight: '1px' }}>
                   <Divider />
@@ -186,12 +158,12 @@ export default function Home() {
                       id="email"
                       size="small"
                       placeholder="example@email.com"
-                      autoComplete='off'
+                      autoComplete="off"
                       fullWidth
                       sx={{
-                        "&:hover .MuiOutlinedInput-notchedOutline": {
-                          borderColor: "primary.main"
-                        }
+                        '&:hover .MuiOutlinedInput-notchedOutline': {
+                          borderColor: 'primary.main',
+                        },
                       }}
                     />
                   </Box>
