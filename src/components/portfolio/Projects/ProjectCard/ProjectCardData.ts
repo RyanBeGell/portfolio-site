c94e2f0c09e81561f77fd79c1fe4e79d539619b8
RyanBeGell <Ryanbegell@outlook.com>
2023-06-24 18:38:35 -0400

    tags added to sample data

--- src/components/portfolio/Projects/ProjectCard/ProjectCardData.ts
+++ src/components/portfolio/Projects/ProjectCard/ProjectCardData.ts
@@ -2,31 +2,37 @@ export default [
     {
         title:"Lizard",
         body: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-        image: "https://animals.sandiegozoo.org/sites/default/files/2017-12/iguana-grand-cayman-blue.jpg"
+        image: "https://animals.sandiegozoo.org/sites/default/files/2017-12/iguana-grand-cayman-blue.jpg",
+        tags:[ 'Java' ],
     },
     {
         title:"Lizard1",
         body: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-        image: "https://animals.sandiegozoo.org/sites/default/files/2017-12/iguana-grand-cayman-blue.jpg"
+        image: "https://animals.sandiegozoo.org/sites/default/files/2017-12/iguana-grand-cayman-blue.jpg",
+        tags:['MaterialUI', 'React', 'TypeScript', 'Java' ],
     },
     {
         title:"Lizard2",
         body: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-        image: "https://animals.sandiegozoo.org/sites/default/files/2017-12/iguana-grand-cayman-blue.jpg"
+        image: "https://animals.sandiegozoo.org/sites/default/files/2017-12/iguana-grand-cayman-blue.jpg",
+        tags:['Node', 'Typescript' ],
     },
     {
         title:"Lizard",
         body: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-        image: "https://animals.sandiegozoo.org/sites/default/files/2017-12/iguana-grand-cayman-blue.jpg"
+        image: "https://animals.sandiegozoo.org/sites/default/files/2017-12/iguana-grand-cayman-blue.jpg",
+        tags:['Spring', 'PostgreSQL' ],
     },
     {
         title:"Lizard1",
         body: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-        image: "https://animals.sandiegozoo.org/sites/default/files/2017-12/iguana-grand-cayman-blue.jpg"
+        image: "https://animals.sandiegozoo.org/sites/default/files/2017-12/iguana-grand-cayman-blue.jpg",
+        tags:['React', 'Node' ],
     },
     {
         title:"Lizard2",
         body: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-        image: "https://animals.sandiegozoo.org/sites/default/files/2017-12/iguana-grand-cayman-blue.jpg"
+        image: "https://animals.sandiegozoo.org/sites/default/files/2017-12/iguana-grand-cayman-blue.jpg",
+        tags:['Golang' ],
     },
 ]
