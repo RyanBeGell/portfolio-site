fdc8e19140f75160f2e09e82752071847f9e560e
RyanBeGell <Ryanbegell@outlook.com>
2023-06-23 23:38:28 -0400

    tag system implemented for portfolio

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,24 +1,57 @@
-import BlogPostsCardData from '@/src/components/blog/Card/BlogCardData';
 import ProjectCard from '@/src/components/portfolio/Projects/ProjectCard/ProjectCard';
 import ProjectData from '@/src/components/portfolio/Projects/ProjectCard/ProjectCardData';
 import { Chip, Pagination, Paper, Typography, useTheme } from '@mui/material';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export default function Portfolio() {
-  //State for Modal
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
 
+  const router = useRouter();
+  const { tag } = router.query;
   const theme = useTheme();
-  const darkMode = theme.palette.mode === 'dark';
+  const [tagActive, setTagActive] = useState<string>('');
+  const [tagSelected, setTagSelected] = useState<boolean>(false);
+  const [selectedChip, setSelectedChip] = useState<string | null>(
+    tag ? String(tag) : null
+  );
 
-  const chipData = 
-    //flattening each blog post's array of chips into a single array of all chips
-    //Afterwards taking the set of that array to get all unique chips
-  BlogPostsCardData.flatMap((post) => post.chips);
+  const handleChipClick = (label: string) => () => {
+    const encodedTag = label.replace(/\s/g, '+');
+
+    if (label === selectedChip) {
+      setSelectedChip(null);
+      router.replace('/portfolio');
+      setTagSelected(false);
+    } else {
+      setSelectedChip(label);
+      router.replace(`/portfolio?tag=${encodedTag}`);
+      setTagSelected(true);
+    }
+  };
+
+  useEffect(() => {
+    if (tag) {
+      setSelectedChip(tag as string);
+      setTagSelected(true);
+    } else {
+      setSelectedChip(null);
+      setTagSelected(false);
+    }
+  }, [tag]);
+
+  const chipData = Array.from(
+    new Set(ProjectData.flatMap((post) => post.tags))
+  );
+
+  const filteredPosts = selectedChip
+    ? ProjectData.filter((post) => post.tags.includes(selectedChip))
+    : ProjectData;
+
+  const darkMode = theme.palette.mode === 'dark';
 
   return (
     <>
@@ -53,63 +86,77 @@ export default function Portfolio() {
               </Typography>
             </Box>
             <Box>
-              <Typography
-                variant={'h4'}
-                className={'neutraface'}
-                sx={{ mb: 3 }}
-              >
-                Projects
-              </Typography>
-              <Grid container spacing={5}>
-                <Grid item xs={12} md={8}>
-                  <Grid container spacing={3}>
-                    {ProjectData.map((item, index) => (
-                      <Grid item xs={12} sm={6} key={index}>
-                        <ProjectCard
-                          handleOpen={handleOpen}
-                          title={item.title}
-                          body={item.body}
-                          image={item.image}
-                        />
-                      </Grid>
-                    ))}
-                  </Grid>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                  <Paper
-                    variant={darkMode ? 'outlined' : undefined}
-                    elevation={8}
-                    sx={{
-                      px: 3,
-                      pb: 3,
-                      mb: 3,
-                    }}
+              {tagSelected ? (
+                <Typography variant={'h4'} className={'neutraface'}>
+                  {'Projects using '}
+                  <Typography
+                    component="span"
+                    variant={'h4'}
+                    className={'neutraface'}
+                    sx={{ color: 'primary.main' }}
                   >
-                    <Typography variant='h6' fontWeight={'400'} sx={{ py: 3 }}>
-                      Filter by tag
-                    </Typography>
-                    {chipData.map((label) => (
-                      <Chip
-                        key={label}
-                        label={label}
-                        clickable
-                        color={'default'}
-                        sx={{ mr: 1, mb: 1, borderRadius: '4px' }}
+                    {`"${selectedChip}"`}
+                  </Typography>
+                </Typography>
+              ) : (
+                <Typography variant={'h4'} className={'neutraface'}>
+                  {'Projects'}
+                </Typography>
+              )}
+            </Box>
+            <Grid container spacing={5} sx={{ pt: '24px' }}>
+              <Grid item xs={12} md={8}>
+                <Grid container spacing={3}>
+                  {filteredPosts.map((item, index) => (
+                    <Grid item xs={12} sm={6} key={index}>
+                      <ProjectCard
+                        handleOpen={handleOpen}
+                        title={item.title}
+                        body={item.body}
+                        image={item.image}
                       />
-                    ))}
-                  </Paper>
+                    </Grid>
+                  ))}
                 </Grid>
               </Grid>
-              <Pagination
-                count={4}
-                shape="rounded"
-                variant="outlined"
-                color='primary'
-                sx={{ mt: 3, '& .MuiPaginationItem-page.Mui-selected': {
+              <Grid item xs={12} md={4}>
+                <Paper
+                  variant={darkMode ? 'outlined' : undefined}
+                  elevation={8}
+                  sx={{
+                    px: 3,
+                    pb: 3,
+                    mb: 3,
+                  }}
+                >
+                  <Typography variant="h6" fontWeight={'400'} sx={{ py: 3 }}>
+                    Filter by tag
+                  </Typography>
+                  {chipData.map((label) => (
+                    <Chip
+                      key={label}
+                      label={label}
+                      clickable
+                      color={label === selectedChip ? 'primary' : 'default'}
+                      onClick={handleChipClick(label)}
+                      sx={{ mr: 1, mb: 1, borderRadius: '4px' }}
+                    />
+                  ))}
+                </Paper>
+              </Grid>
+            </Grid>
+            <Pagination
+              count={4}
+              shape="rounded"
+              variant="outlined"
+              color="primary"
+              sx={{
+                mt: 3,
+                '& .MuiPaginationItem-page.Mui-selected': {
                   color: 'text.primary',
-                }, }}
-              />
-            </Box>
+                },
+              }}
+            />
           </Box>
         </Box>
       </Box>
