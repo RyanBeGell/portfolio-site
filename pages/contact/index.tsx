9e16be2f5fb371d0c4a33a52b0a2686d50ac8905
RyanBeGell <Ryanbegell@outlook.com>
2023-06-28 23:03:18 -0400

    contact form added

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,13 +1,29 @@
-import { Box, Typography } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+import { Box, Button, TextField, Typography, useTheme } from '@mui/material';
 import Link from '@mui/material/Link';
+import { useState } from 'react';
 
 export default function Contact() {
-  const styles = {
-    link: {
-      '&:hover': {
-        color: 'red',
-      },
-    },
+  const theme = useTheme();
+
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formData); // will be changed to AWS SES
   };
 
   return (
@@ -36,17 +52,28 @@ export default function Contact() {
                   variant={'h3'}
                   component={'span'}
                   className={'neutraface'}
-                  sx={{ color: 'primary.main'  }}
+                  color={`${
+                    theme.palette.mode === 'dark' ? 'primary.light' : undefined
+                  }`}
+                  sx={
+                    theme.palette.mode === 'light'
+                      ? {
+                          background: `linear-gradient(to right, ${theme.palette.primary.light}, ${theme.palette.primary.dark})`,
+                          WebkitBackgroundClip: 'text',
+                          WebkitTextFillColor: 'transparent',
+                        }
+                      : {}
+                  }
                 >
                   connect
                 </Typography>{' '}
                 with me
               </Typography>
-              <Typography variant="subtitle2" color="text.secondary">
+              <Typography variant="body2" color="text.secondary">
                 Fill in the contact form below or contact me anytime at{' '}
                 <Link
                   href="#"
-                  color='inherit'
+                  color="inherit"
                   sx={{
                     '&:hover': {
                       color: 'primary.main',
@@ -59,6 +86,103 @@ export default function Contact() {
               </Typography>
             </Box>
           </Box>
+        </Box>
+        <Box component="form" noValidate onSubmit={handleSubmit}>
+          <TextField
+            margin="normal"
+            id="First Name"
+            label="First Name"
+            name="firstName"
+            autoComplete="off"
+            value={formData.firstName}
+            onChange={handleInputChange}
+            sx={{
+              width: '49%',
+              mr: '2%',
+              '&:hover  .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline':
+                {
+                  borderColor: 'primary.main',
+                },
+            }}
+            color="primary"
+          />
+          <TextField
+            margin="normal"
+            id="Last Name"
+            label="Last Name"
+            name="lastName"
+            autoComplete="off"
+            value={formData.lastName}
+            onChange={handleInputChange}
+            sx={{
+              width: '49%',
+              '&:hover  .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline':
+                {
+                  borderColor: 'primary.main',
+                },
+            }}
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            autoComplete="off"
+            id="email"
+            label="Email Address"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            sx={{
+              '&:hover  .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline':
+                {
+                  borderColor: 'primary.main',
+                },
+            }}
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            autoComplete="off"
+            id="subject"
+            label="Subject"
+            name="subject"
+            value={formData.subject}
+            onChange={handleInputChange}
+            sx={{
+              '&:hover  .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline':
+                {
+                  borderColor: 'primary.main',
+                },
+            }}
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            multiline
+            rows={6}
+            autoComplete="off"
+            id="message"
+            label="Your message here"
+            name="message"
+            value={formData.message}
+            onChange={handleInputChange}
+            sx={{
+              '&:hover  .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline':
+                {
+                  borderColor: 'primary.main',
+                },
+            }}
+          />
+          <Button
+            size="large"
+            variant={`${
+              theme.palette.mode == 'dark' ? 'outlined' : 'contained'
+            }`}
+            sx={{ mt: '16px' }}
+            startIcon={<SendIcon />}
+            type="submit"
+          >
+            Send Message
+          </Button>
         </Box>
       </Box>
     </>
