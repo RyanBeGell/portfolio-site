c7ba8c3e2fb6cd2273ffba3fa7279ca58942fd87
RyanBeGell <Ryanbegell@outlook.com>
2023-06-28 23:04:47 -0400

    pallette tweaks

--- src/theme/createPallette.ts
+++ src/theme/createPallette.ts
@@ -1,4 +1,4 @@
-import { PaletteMode } from "@mui/material";
+import { PaletteMode } from '@mui/material';
 
 //Custom Pallette for Light/Dark mode theme
 export const getDesignTokens = (mode: PaletteMode) => ({
@@ -6,16 +6,16 @@ export const getDesignTokens = (mode: PaletteMode) => ({
     mode,
     primary: {
       ...(mode === 'dark'
-      ? {
-          main: '#2196F3',
-          light: '#0072E5',
-          dark: '#007FFF',
-        }
-      : {
-          main: '#0d6efd',
-          light: '#0072E5',
-          dark: '#007FFF',
-      }),
+        ? {
+            main: '#2196F3',
+            light: '#66b2ff',
+            dark: '#007FFF',
+          }
+        : {
+            main: '#0d6efd',
+            light: '#2196F3',
+            dark: '#0059b2',
+          }),
     },
     secondary: {
       main: '#f50057',
@@ -37,64 +37,62 @@ export const getDesignTokens = (mode: PaletteMode) => ({
     },
 
     background: {
-    ...(mode === 'dark'
-    ? {
-        default: '#0A1929',
-        dark: '#0A1929',
-        // dark: '#091725',
-        paper: '#001e3c',
-        nav: '#001e3c',
-        blogNav:'rgba(0, 30, 60, .7)',
-        paperDivider:'rgb(19, 47, 76)',
-      }
-    : {
-        default: '#f6f9fb',
-        light: '#FFFFFF',
-        paper: '#FFFFFF',
-        nav: '#0d6efd',
-        blogNav: 'rgba(255,255,255, .7)',
-        paperDivider:'rgb(224, 227, 231)',
-    }),
-  },  
+      ...(mode === 'dark'
+        ? {
+            default: '#0A1929',
+            dark: '#0A1929',
+            // dark: '#091725',
+            paper: '#001e3c',
+            nav: '#001e3c',
+            blogNav: 'rgba(0, 30, 60, .7)',
+            paperDivider: 'rgb(19, 47, 76)',
+          }
+        : {
+            default: '#f6f9fb',
+            light: '#FFFFFF',
+            paper: '#FFFFFF',
+            nav: '#0d6efd',
+            blogNav: 'rgba(255,255,255, .7)',
+            paperDivider: 'rgb(224, 227, 231)',
+          }),
+    },
     text: {
       ...(mode === 'dark'
-      ? {
-          primary: '#ffffff',
-          contrast: '#2196F3',
-          secondary: 'rgba(255, 255, 255, 0.7)',
-          navFooter:'#bfbfbf',
-          blogIcons: 'rgba(255, 255, 255, 0.7)',
-        }
-      : {
-          primary: '#000000',
-          contrast: '#114b7a',
-          blogIcons: 'rgba(168, 168, 168, 1)',
-          secondary:'rgba(117, 117, 117, 1)',
-          navFooter:'#e0e0e0',
-      }),
+        ? {
+            primary: '#ffffff',
+            contrast: '#2196F3',
+            secondary: 'rgba(255, 255, 255, 0.7)',
+            navFooter: '#bfbfbf',
+            blogIcons: 'rgba(255, 255, 255, 0.7)',
+          }
+        : {
+            primary: '#000000',
+            contrast: '#114b7a',
+            blogIcons: 'rgba(168, 168, 168, 1)',
+            secondary: '#3e5060',
+            navFooter: '#e0e0e0',
+          }),
     },
     typography: {
       ...(mode === 'dark'
-      ? {
-        p:{
-          color:'rgba(255, 255, 255, 0.7) !important'
-        }
-      }
-      : {
-
-      }),
+        ? {
+            p: {
+              color: 'rgba(255, 255, 255, 0.7) !important',
+            },
+          }
+        : {}),
     },
 
     action: {
       ...(mode === 'dark'
-      ? {
-        hover: '#0072E540',
-        arrow: '#0072E52B'
-        }
-      : {
-        hover: '#0254cdAD',
-        arrow: '#0d6efd12'
-      }),
+        ? {
+            hover: '#0072E540',
+            arrow: '#0072E52B',
+          }
+        : {
+            hover: '#0254cdAD',
+            arrow: '#0d6efd12',
+          }),
     },
   },
   //  Override MUI from changing paper opacity automatically in dark mode based on elevation
@@ -102,5 +100,5 @@ export const getDesignTokens = (mode: PaletteMode) => ({
     MuiPaper: {
       styleOverrides: { root: { backgroundImage: 'unset' } },
     },
-  },   
+  },
 });
