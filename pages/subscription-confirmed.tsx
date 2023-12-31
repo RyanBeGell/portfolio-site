aba410fb3d146af87d1efeffa996b25d2a66931f
RyanBeGell <Ryanbegell@outlook.com>
2023-12-31 18:05:16 -0500

    Add SubscriptionConfirmed page for blog

---
+++ pages/subscription-confirmed.tsx
@@ -0,0 +1,81 @@
+import MarkEmailReadIcon from '@mui/icons-material/MarkEmailRead';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import { Box, Divider, Paper, Typography, useTheme } from '@mui/material';
+import Button from '@mui/material/Button';
+import MUILink from '@mui/material/Link';
+import Image from 'next/image';
+
+export default function SubscriptionConfirmed() {
+  const theme = useTheme();
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      textAlign="center"
+      sx={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
+      <Paper
+        variant="outlined"
+        sx={{
+          maxWidth: '500px',
+          maxHeight: '600px',
+          mt: 4, // Adjust this value to account for the navbar height (56px / 2 = 28px)
+        }}
+      >
+        <Box sx={{ p: 4 }}>
+          <MarkEmailReadIcon
+            sx={{ fontSize: '64px', fontColor: '#66bb6a' }}
+            color="primary"
+          />
+          <Typography variant={'h5'} fontWeight="700" sx={{ mb: 4 }}>
+            Thanks for subscribing!
+          </Typography>
+          <Typography variant="body1">
+            You have been successfully added to the blog subscriber list. You
+            will now receive email notifications for new posts and updates to my
+            blog.
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<NavigateBeforeIcon />}
+            sx={{ my: 4, textTransform: 'none' }}
+          >
+            Return to blog
+          </Button>
+          <Divider sx={{ mb: 4 }} />
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Image
+              height={25}
+              width={25}
+              src="/favicon.png"
+              style={{ cursor: 'pointer' }}
+            />
+            <Typography variant={'body2'} className={'neutraface'}>
+              &nbsp;&nbsp; Ryan BeGell
+            </Typography>
+          </Box>
+        </Box>
+        {/* typography paragraph */}
+      </Paper>
+      <Typography variant={'subtitle2'} color="text.secondary" sx={{ pt: 4 }}>
+        Changed your mind? If you wish to unsubscribe,{' '}
+        <MUILink color="inherit"> click here.</MUILink>
+      </Typography>
+    </Box>
+  );
+}
