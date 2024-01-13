cf7b9af2585894e2b81e3fa9f950d03478de8597
RyanBeGell <Ryanbegell@outlook.com>
2024-01-12 23:05:27 -0500

    links to online resume and project portfolio in AboutMe component

--- src/components/portfolio/About/AboutMe.tsx
+++ src/components/portfolio/About/AboutMe.tsx
@@ -1,17 +1,47 @@
-import { Box } from '@mui/material';
+import { Box, useTheme } from '@mui/material';
+import Link from 'next/link';
 import ScrollAnimation from '../../ScrollAnimation';
 import SectionTitle from '../SectionTitle/SectionTitle';
 
 export default function AboutMe() {
+  const theme = useTheme();
   return (
     <Box className="centerBox">
       <SectionTitle title="About Me" />
       <ScrollAnimation animation={'fade'} timeout={1000}>
         {/* Div Necessary to provide react element to Scroll Animation */}
         <div>
-          {
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida elit sit amet mauris consequat ullamcorper. Nulla sed molestie purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer leo felis, mattis quis dui vel, eleifend placerat purus. Etiam rhoncus odio metus, ut commodo sem sodales ac. Vivamus purus metus, varius pharetra maximus vitae, pellentesque nec lacus. Nullam condimentum ut augue at posuere. Praesent mattis justo turpis, in ullamcorper purus sagittis non. Sed tellus dolor, blandit eget ante a, convallis volutpat massa. '
-          }
+          {`I'm a self-motivated Full-Stack Software Engineer, with a natural curiosity for learning new technologies and a proven track record in rapid learning and independent problem-solving. Actively seeking a challenging role to collaborate, make meaningful contributions, and further expand my skills and experience. I enjoy working with ReactJS, Java, and AWS to build full stack applications, but I'm ready eager to get started learning a new language, framework, or technology. Want to find out more about my experience? Check out my `}
+          <Box
+            display={'inline'}
+            sx={{
+              '& a': {
+                color: 'primary.main',
+                textDecoration: 'none', // Remove the underline by default
+                '&:hover': {
+                  textDecoration: 'underline', // Only show underline on hover
+                },
+              },
+            }}
+          >
+            <Link href="/resume">online resume</Link>{' '}
+          </Box>
+          {`and `}
+          <Box
+            display={'inline'}
+            sx={{
+              '& a': {
+                color: 'primary.main',
+                textDecoration: 'none', 
+                '&:hover': {
+                  textDecoration: 'underline', 
+                },
+              },
+            }}
+          >
+            <Link href="/portfolio">project portfolio</Link>
+            {'.'}
+          </Box>
         </div>
       </ScrollAnimation>
     </Box>
